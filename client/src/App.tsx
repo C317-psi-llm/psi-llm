7f2bcba9ce7b5e74b534826f3169825964dd866b
FranciscoGSilverio <franciscogsilverio@gmail.com>
2026-03-27 15:05:37 -0300

    Add frontend proj

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-black-600">
+      Hello world with <strong className="underline">Tailwind CSS</strong>
+    </h1>
+  )
+}
